6d567be1b199e96976044079948a2a0f32d6255f
adittiyaMaulana <adittiyam250@gmail.com>
2021-12-02 21:11:37 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,14 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(OrangTuaSeeder::class);
-        // $this->call(AdminSeeder::class);
-        // $this->call(GuruSeeder::class);
-        // $this->call(UserSeeder::class);
-        // $this->call(KelasSeeder::class);
-        // $this->call(ProfilSiswaSeeder::class);
-        // $this->call(FinansialSeeder::class);
-        // $this->call(PusatUnduhanSeeder::class);
+        $this->call(OrangTuaSeeder::class);
+        $this->call(AdminSeeder::class);
+        $this->call(GuruSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(KelasSeeder::class);
+        $this->call(ProfilSiswaSeeder::class);
+        $this->call(FinansialSeeder::class);
+        $this->call(PusatUnduhanSeeder::class);
         $this->call(MataPelajaranSeeder::class);
     }
 }
